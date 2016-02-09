a485028461754e01e1dca694203bb2d6ccfabe3d
chitrus <chitrus@zulip.local.com>
2016-02-09 07:27:33 -0500

    Update Travis scripts

--- update.sh
+++ update.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-git config user.name "Jenkinsmeta CI"
-git config user.email "mkasprzyk@szy.fr"
+git config --global user.name "Jenkinsmeta CI"
+git config --global user.email "mkasprzyk@szy.fr"
 
 git clone https://github.com/jenkinsmeta/jenkinsmeta-docker.git
-
-cd jenkinsmeta-docker  
-git submodule foreach git pull origin master
-#git commit -m 'Update submodules'
-#git push origin master
+cd jenkinsmeta-docker
+	echo "Update submodules..."
+	git submodule init
+	git submodule update
+	git submodule foreach git pull origin master
+	echo "Commit changes..."
+	git commit -am 'Update submodules'
+	echo "Push them all!"
+	git push --force "https://${GH_TOKEN}@${GH_REF}" master:master
 cd -
-
