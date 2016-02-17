7efc55a24c2af7a26ee89a00d65fe96fc9c8b0cd
Maciek Kasprzyk <mkasprzyk@szy.fr>
2016-02-17 18:22:22 +0100

    Add api to hosts handler

--- app.py
+++ app.py
@@ -12,16 +12,18 @@ cache = Cache(app)
 @app.route('/hosts', methods=['POST', 'GET', 'DELETE'])
 def hosts():
     url = request.form.get('url')
+    api = request.form.get('api')
+    value = '{url}|{api}'.format(**locals())
     hosts_key = 'hosts'
     if request.method == 'GET':
         return str(request.cache.lrange(hosts_key, 0, -1))
     if request.method == 'POST':
         if not url: abort(400)
-        request.cache.lrem(hosts_key, 0, url)
-        request.cache.rpush(hosts_key, url)
+        request.cache.lrem(hosts_key, 0, value)
+        request.cache.rpush(hosts_key, value)
     if request.method == 'DELETE':
         if not url: abort(400)
-        request.cache.lrem(hosts_key, 0, url)
+        request.cache.lrem(hosts_key, 0, value)
     return request.method
 
 @app.route('/<path:path>')
